d613feb0b60b0714d95ccfbbb91a542091eaf9ba
Jane Moroz <Moroz.jane@yahoo.com>
2023-03-24 14:47:27 +0300

    fix: fix viewport height problem

--- flying-bananas/src/Experience.jsx
+++ flying-bananas/src/Experience.jsx
@@ -16,20 +16,21 @@ function Box({ z }) {
   const ref = useRef();
 
   // Get viewport width and height
-  const { viewport } = useThree();
+  const { viewport, camera } = useThree();
+  const { width, height } = viewport.getCurrentViewport(camera, [0, 0, z]);
 
   // Set x position to random value
   const [data] = useState({
     x: THREE.MathUtils.randFloatSpread(2),
-    y: THREE.MathUtils.randFloatSpread(viewport.height),
+    y: THREE.MathUtils.randFloatSpread(height),
   });
 
   useFrame((state) => {
     // Set x position to random value according to viewport width
-    ref.current.position.set(data.x * viewport.width, (data.y += 0.1), z);
+    ref.current.position.set(data.x * width, (data.y += 0.1), z);
 
-    if (data.y > viewport.height / 1.5) {
-      data.y = -viewport.height / 1.5;
+    if (data.y > height / 1.5) {
+      data.y = -height / 1.5;
     }
   });
 
