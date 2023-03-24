9ff8173a6c033c6f85453ccf66cf95b92328f349
Jane Moroz <Moroz.jane@yahoo.com>
2023-03-24 16:29:07 +0300

    feat: add final animated background

--- flying-bananas/src/Experience.jsx
+++ flying-bananas/src/Experience.jsx
@@ -2,11 +2,11 @@ import { useRef, useState } from "react";
 
 import { useGLTF } from "@react-three/drei";
 
-// Drei helpers
-import { OrbitControls, Environment } from "@react-three/drei";
+// PostProcessing
+import { EffectComposer, DepthOfField } from "@react-three/postprocessing";
 
-// Perf
-import { Perf } from "r3f-perf";
+// Drei helpers
+import { Environment } from "@react-three/drei";
 
 // Animation
 import { useFrame, useThree } from "@react-three/fiber";
@@ -14,8 +14,10 @@ import { useFrame, useThree } from "@react-three/fiber";
 // Three
 import * as THREE from "three";
 
-function Box({ z }) {
+function Banana({ z }) {
   const ref = useRef();
+
+  const { nodes, materials } = useGLTF("/banana-v1-transformed.glb");
 
   // Get viewport width and height
   const { viewport, camera } = useThree();
@@ -25,49 +27,52 @@ function Box({ z }) {
   const [data] = useState({
     x: THREE.MathUtils.randFloatSpread(2),
     y: THREE.MathUtils.randFloatSpread(height),
+    rX: Math.random() * Math.PI,
+    rY: Math.random() * Math.PI,
+    rZ: Math.random() * Math.PI,
   });
 
   useFrame((state) => {
-    // Set x position to random value according to viewport width
-    ref.current.position.set(data.x * width, (data.y += 0.1), z);
+    // Set random rotation
+    ref.current.rotation.set(
+      (data.rX += 0.001),
+      (data.rY += 0.001),
+      (data.rZ += 0.001)
+    );
 
-    if (data.y > height / 1.5) {
-      data.y = -height / 1.5;
+    // Set x position to random value according to viewport width
+    ref.current.position.set(data.x * width, (data.y += 0.025), z);
+
+    if (data.y > height) {
+      data.y = -height;
     }
   });
 
   return (
-    <mesh ref={ref}>
-      <boxGeometry />
-      <meshBasicMaterial color="hotpink" />
-    </mesh>
+    <mesh
+      ref={ref}
+      geometry={nodes.banana.geometry}
+      material={materials.skin}
+      material-emissive="orange"
+    />
   );
 }
 
-// Banana
-export function Banana(props) {
-  const { nodes, materials } = useGLTF("/banana-v1-transformed.glb");
-  return (
-    <group {...props} dispose={null}>
-      <mesh
-        geometry={nodes.banana.geometry}
-        material={materials.skin}
-        rotation={[-Math.PI / 2, 0, 0]}
-        material-emissive="orange"
-      />
-    </group>
-  );
-}
-
-function App({ count = 100 }) {
+function App({ count = 100, depth = 80 }) {
   return (
     <>
-      <Perf position="top-left" />
-      <Banana scale={0.5} />
       <Environment preset="sunset" />
-      {/* {Array.from({ length: count }, (_, i) => (
-        <Box key={i} z={-i} />
-      ))} */}
+      {Array.from({ length: count }, (_, i) => (
+        <Banana key={i} z={-(i / count) * depth - 20} />
+      ))}
+      <EffectComposer>
+        <DepthOfField
+          target={[0, 0, depth / 2]}
+          focalLength={0.5}
+          bokehScale={11}
+          height={700}
+        />
+      </EffectComposer>
     </>
   );
 }
--- flying-bananas/src/index.jsx
+++ flying-bananas/src/index.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Suspense } from "react";
 
+// Perf
+import { Perf } from "r3f-perf";
+
 // Canvas
 import { Canvas } from "@react-three/fiber";
 
@@ -12,9 +15,17 @@ import "./style.css";
 import Experience from "./Experience.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Canvas>
-    <ambientLight intensity={0.2} />
-    <spotLight position={[10, 10, 10]} intensity={2} />
+  <Canvas gl={{ alpha: false }} camera={{ near: 0.01, far: 110, fov: 30 }}>
+    <color attach="background" args={["#ffbf40"]} />
+
+    {/* Perf */}
+    <Perf position="top-left" />
+
+    {/* Lights */}
+    {/* <ambientLight intensity={0.2} /> */}
+    <spotLight position={[10, 10, 10]} intensity={1} />
+
+    {/* Experience */}
     <Suspense fallback={null}>
       <Experience />
     </Suspense>
